0e9ac82731b410b1304d477ccf752e6120688ac5
ggehrer <g.gehrer@hotmail.com>
2023-02-03 21:01:55 +0100

    Changed delete button style.

--- QTChinnok.MvvMApp/ViewModels/MediaTypeViewModel.cs
+++ QTChinnok.MvvMApp/ViewModels/MediaTypeViewModel.cs
@@ -1,26 +1,29 @@
-﻿using QTChinnok.Logic.Contracts;
-using System.Linq;
+﻿using System.Linq;
+using QTChinnok.Logic.Contracts;
 
 namespace QTChinnok.MvvMApp.ViewModels
 {
     using TModel = Models.MediaType;
     using TEntity = Logic.Models.Base.MediaType;
+
     public class MediaTypeViewModel : ModelViewModel<TModel, TEntity>
     {
         public string Name
         {
             get => Model.Name;
-            set => Model.Name = value;
+            set
+            {
+                Model.Name = value;
+                OnPropertiesChanged();
+            }
         }
-
         public override IDataAccess<TEntity> CreateController()
         {
             return new Logic.Controllers.Base.MediaTypesController();
         }
-
         protected override void OnPropertiesChanged(params string[] propertyNames)
         {
-            base.OnPropertiesChanged(propertyNames.Union(new string[] { nameof(Id), nameof(Name) }).ToArray());
+            base.OnPropertiesChanged(propertyNames.Union(new string[] { "Id", "Name" }).ToArray());
         }
     }
 }
